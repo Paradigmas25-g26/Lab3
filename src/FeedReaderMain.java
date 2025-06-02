88476f9098a35bb00ac789a83cfff293eedd4091
Lilen <lilen.bena@mi.unc.edu.ar>
2025-06-02 18:16:11 +0000

    Refactor FeedReaderMain, Article, Feed, and NamedEntity classes for improved structure and functionality

--- src/FeedReaderMain.java
+++ src/FeedReaderMain.java
@@ -2,7 +2,11 @@
 import feed.*;
 import httpRequest.*;
 import org.apache.spark.sql.SparkSession;
-import namedEntity.heuristic.*;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.api.java.function.MapPartitionsFunction;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.MapFunction;
 import parser.*;
 import subscription.*;
 import java.util.List;
@@ -33,64 +37,96 @@ public class FeedReaderMain {
             return;
         }
         subscriptionList = s.getSubscriptionsList();
-        Feed feed = null;
+        List<String> urls = new ArrayList<>();
+        Feed feedDS = null;
 
         for (SingleSubscription singSub : subscriptionList) {
-            // Llamar al httpRequester para obtener el feed del servidor
-            httpRequester http = new httpRequester();
-
-            if (!singSub.getUrlType().equals("rss")) {
-                // si entramos es porque NO es un rss
-                System.out.println("Unable to parse feeds different from RSS format.");
-                System.out.println("Attempted to parse feed type: " + singSub.getUrlType());
-                continue;
-            }
-
             for (int i = 0; i < singSub.getUlrParamsSize(); i++) {
-                String urlFeed = singSub.getFeedToRequest(i);
-                String feedContent = http.getFeedRss(urlFeed);
-                RssParser rssParser = new RssParser();
-                feed = rssParser.parser(feedContent);
-
-                if (feed != null) {
-                    feeds.add(feed);
-                }
+                urls.add(singSub.getFeedToRequest(i));
             }
         }
-
-        if (args.length == 0) {
-            System.out.println("printing without -ne");
-
-            for (Feed feedaux : feeds) {
-                feedaux.prettyPrint();
-                System.out.println("------------------------");
-                System.out.println("|feed :" + feedaux.getSiteName() + "|");
-                System.out.println("------------------------");
-            }
-
-        } else if (args.length == 1) {
-            if (!args[0].equals("-ne")) {
-                printHelp();
-                return;
-            }
-
-            System.out.println("printing with -ne");
-            QuickHeuristic qh = new QuickHeuristic();
-
-            for (Feed feedaux : feeds) {
-                for (Article a : feedaux.getArticleList()) {
-                    a.computeNamedEntities(qh);
+        Dataset<String> urlsDS = spark.createDataset(urls, Encoders.STRING());
+        Dataset<Feed> feedsDS = urlsDS.mapPartitions((MapPartitionsFunction<String, Feed>) iterator -> {
+            httpRequest.httpRequester requester = new httpRequest.httpRequester();
+            parser.RssParser parser = new parser.RssParser();
+            List<Feed> results = new ArrayList<>();
+            while (iterator.hasNext()) {
+                String url = iterator.next();
+                String feedContent = requester.getFeedRss(url);
+                if (feedContent != null) {
+                    Feed feed = parser.parser(feedContent);
+                    if (feed != null) {
+                        results.add(feed);
+                    }
                 }
-                feedaux.addAllNamedEntities();
-                feedaux.prettyPrintAllNamedEntities();
-                System.out.println("------------------------");
-                System.out.println("|feed :" + feedaux.getSiteName() + "|");
-                System.out.println("------------------------");
             }
+            return results.iterator();
+        }, Encoders.javaSerialization(Feed.class));
 
-        } else {
-            printHelp();
-        }
+        // 3. Extraer todos los artículos de todos los feeds
+        Dataset<Article> articlesDS = feedsDS.flatMap((FlatMapFunction<Feed, Article>) feed -> feed.getArticleList().iterator(),
+                Encoders.javaSerialization(Article.class)
+        );
+
+        // 4. Procesar entidades nombradas en paralelo
+        Dataset<Article> processedArticlesDS = articlesDS.map((MapFunction<Article, Article>) article -> {
+            namedEntity.heuristic.QuickHeuristic qh = new namedEntity.heuristic.QuickHeuristic();
+            article.computeNamedEntities(qh);
+            return article;
+        }, Encoders.javaSerialization(Article.class));
+
+        // 5. Recoger los resultados si los necesitas en memoria
+        List<Article> processedArticles = processedArticlesDS.collectAsList();
+
+        // Aquí puedes imprimir, guardar o seguir procesando los artículos
+        // processedArticles.forEach(System.out::println);
+        // for (SingleSubscription singSub : subscriptionList) {
+        //     // Llamar al httpRequester para obtener el feed del servidor
+        //     httpRequester http = new httpRequester();
+        //     if (!singSub.getUrlType().equals("rss")) {
+        //         // si entramos es porque NO es un rss
+        //         System.out.println("Unable to parse feeds different from RSS format.");
+        //         System.out.println("Attempted to parse feed type: " + singSub.getUrlType());
+        //         continue;
+        //     }
+        //     for (int i = 0; i < singSub.getUlrParamsSize(); i++) {
+        //         String urlFeed = singSub.getFeedToRequest(i);
+        //         String feedContent = http.getFeedRss(urlFeed);
+        //         RssParser rssParser = new RssParser();
+        //         feed = rssParser.parser(feedContent);
+        //         if (feed != null) {
+        //             feeds.add(feed);
+        //         }
+        //     }
+        // }
+        // if (args.length == 0) {
+        //     System.out.println("printing without -ne");
+        //     for (Feed feedaux : feeds) {
+        //         feedaux.prettyPrint();
+        //         System.out.println("------------------------");
+        //         System.out.println("|feed :" + feedaux.getSiteName() + "|");
+        //         System.out.println("------------------------");
+        //     }
+        // } else if (args.length == 1) {
+        //     if (!args[0].equals("-ne")) {
+        //         printHelp();
+        //         return;
+        //     }
+        //     System.out.println("printing with -ne");
+        //     QuickHeuristic qh = new QuickHeuristic();
+        //     for (Feed feedaux : feeds) {
+        //         for (Article a : feedaux.getArticleList()) {
+        //             a.computeNamedEntities(qh);
+        //         }
+        //         feedaux.addAllNamedEntities();
+        //         feedaux.prettyPrintAllNamedEntities();
+        //         System.out.println("------------------------");
+        //         System.out.println("|feed :" + feedaux.getSiteName() + "|");
+        //         System.out.println("------------------------");
+        //     }
+        //} else {
+        //  printHelp();
+        //}
         spark.stop(); // Cerrar la sesión al final
     }
 }
--- src/namedEntity/NamedEntity.java
+++ src/namedEntity/NamedEntity.java
@@ -1,89 +1,87 @@
 package namedEntity;
 
-import namedEntity.topic.*;
+import java.io.Serializable;
 import java.util.Map;
+import namedEntity.topic.*;
+
 /*Esta clase modela la nocion de entidad nombrada*/
+public class NamedEntity implements Serializable {
 
-public class NamedEntity {
-	String name;
-	String category; 
-	int frequency;
-	Topic topic;
-	private static Map<String, String> topicMap = Map.ofEntries(
-		Map.entry("Microsoft", "Tecnology"),
-		Map.entry("Apple", "Tecnology"),
-		Map.entry("Google", "Tecnology"),
-		Map.entry("Musk", "Tecnology"),
-		Map.entry("Biden", "International"),
-		Map.entry("Trump", "International"),
-		Map.entry("Messi", "Futbol"),
-		Map.entry("Federer", "Tenis"),
-		Map.entry("USA", "Politics"),
-		Map.entry("Russia", "Politics"),
-		Map.entry("Apple's", "Tecnology"),
-		Map.entry("Trump's", "International"),
-		Map.entry("Musk's", "Tecnology"),
-		Map.entry("AI", "Tecnology")
-		);
-	
-	
-	public NamedEntity(String name, String category, int frequency) {
-		super();
-		this.name = name;
-		this.category = category;
-		this.frequency = frequency;
-	}
+    String name;
+    String category;
+    int frequency;
+    Topic topic;
+    private static Map<String, String> topicMap = Map.ofEntries(
+            Map.entry("Microsoft", "Tecnology"),
+            Map.entry("Apple", "Tecnology"),
+            Map.entry("Google", "Tecnology"),
+            Map.entry("Musk", "Tecnology"),
+            Map.entry("Biden", "International"),
+            Map.entry("Trump", "International"),
+            Map.entry("Messi", "Futbol"),
+            Map.entry("Federer", "Tenis"),
+            Map.entry("USA", "Politics"),
+            Map.entry("Russia", "Politics"),
+            Map.entry("Apple's", "Tecnology"),
+            Map.entry("Trump's", "International"),
+            Map.entry("Musk's", "Tecnology"),
+            Map.entry("AI", "Tecnology")
+    );
 
-	public String getName() {
-		return name;
-	}
+    public NamedEntity(String name, String category, int frequency) {
+        super();
+        this.name = name;
+        this.category = category;
+        this.frequency = frequency;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setTopic(Topic topic){
-		this.topic = topic;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getCategory() {
-		return this.category;
-	}
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
 
-	public String getTopic(){
-		return this.topic.getName();
-	}
+    public String getCategory() {
+        return this.category;
+    }
 
-	public void setCategory(String category) {
-		this.category= category;
-	}
+    public String getTopic() {
+        return this.topic.getName();
+    }
 
-	public int getFrequency() {
-		return frequency;
-	}
+    public void setCategory(String category) {
+        this.category = category;
+    }
 
-	public String getTopicc(String entity){
-		return topicMap.getOrDefault(entity, "Other");
-	}
+    public int getFrequency() {
+        return frequency;
+    }
 
-	public void setFrequency(int frequency) {
-		this.frequency = frequency;
-	}
+    public String getTopicc(String entity) {
+        return topicMap.getOrDefault(entity, "Other");
+    }
 
-	public void incFrequency() {
-		this.frequency++;
-	}
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
 
-	@Override
-	public String toString() {
-		return "ObjectNamedEntity [name=" + name + ", frequency=" + frequency + "]";
-	}
-	public void prettyPrint(){
-		System.out.println(this.getName() + " " + this.getFrequency());
-	}
-	
-	
+    public void incFrequency() {
+        this.frequency++;
+    }
+
+    @Override
+    public String toString() {
+        return "ObjectNamedEntity [name=" + name + ", frequency=" + frequency + "]";
+    }
+
+    public void prettyPrint() {
+        System.out.println(this.getName() + " " + this.getFrequency());
+    }
+
 }
-
-
-
