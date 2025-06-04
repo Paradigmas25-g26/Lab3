5b01f7be12b83bb1224d05b6fc956d810e2fcdcd
Nicolas <nicolas.mansutti@mi.unc.edu.ar>
2025-06-04 10:49:13 -0300

    fst naive implementation

--- src/parser/RssParser.java
+++ src/parser/RssParser.java
@@ -38,6 +38,13 @@ public class RssParser extends GeneralParser {
 
 			// Normalizar el documento
 			doc.getDocumentElement().normalize();
+			
+			Element elementO = doc.getDocumentElement();
+			System.out.println("Root element name is "+elementO.getTagName());
+			if (elementO.getTagName().equals("feed")){
+				AtomParser atom = new AtomParser();
+				return atom.parser(archXML);
+			} 
 
 			//obtener el nombre del sitio                   
 			NodeList seteList = doc.getElementsByTagName("channel");
@@ -102,6 +109,94 @@ public class RssParser extends GeneralParser {
 		}
 		return fecha;
 	}
+
+protected class AtomParser {
+	
+	@Override
+	public feed.Feed parser(String archXML) {
+		//archXML mal formado desde httpRequest
+		if(archXML == null || archXML.equals("")){return null;}
+		try {
+			ByteArrayInputStream file = new ByteArrayInputStream( archXML.getBytes("UTF-8"));
+
+			// Crear el parser DOM
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse(file);
+
+			// Normalizar el documento
+			doc.getDocumentElement().normalize();
+
+			//obtener el nombre del sitio                   
+			/*NodeList seteList = doc.getElementsByTagName("channel");
+			Node nodeForName = seteList.item(0);
+			String name = ((Element)nodeForName).getElementsByTagName("title").item(0).getTextContent();
+			*/
+
+			Element elementO = doc.getDocumentElement();
+			System.out.println("Root element name is "+elementO.getTagName());
+			feed.Feed f = new feed.Feed("ALGUN NOMBRE ");
+
+			// Obtener todos los elementos para el feed 
+			NodeList listItems = doc.getElementsByTagName("entry");
+
+			// Recorrer los items
+			for (int i = 0; i < listItems.getLength(); i++) {
+				Node node = listItems.item(i);
+
+				if (node.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element) node;
+
+					String title = element.getElementsByTagName(TITLE).item(0).getTextContent();
+					String desc = null;
+					try{
+						desc= element.getElementsByTagName("subtitle").item(0).getTextContent();
+					}catch(NullPointerException e){desc= "Sin descripcion"; }
+					String date = element.getElementsByTagName("updated").item(0).getTextContent();
+					String link = element.getElementsByTagName(LINK).item(0).getTextContent();
+					Date fecha = parsingDate(date);
+
+					feed.Article article= new feed.Article(title, desc, fecha, link);
+					f.addArticle(article);
+				}
+			}
+			return f;
+		}
+		catch (ParserConfigurationException | SAXException e) {
+			//el xml que devuelve parser no está bien formado
+			System.out.println("El archivo no está bien configurado " + e);
+			return null;
+		}
+		catch (IOException e) {
+			System.out.println("El archivo no existe o no se puede acceder" + e);
+			return null;
+		}
+		catch (NullPointerException e) {
+			//getElementsByTagName, si no está esa tag, hacer item(0) nos lleva aca.
+			System.out.println("El archivo no contiene info que podemos proveer. " + e);
+			return null;
+		}
+		catch (Exception e) {
+			System.out.println("Problema desconocido. Vuelva a intentarlo. " + e);
+			return null;
+		}
+	}
+	private static  Date parsingDate(String date){
+		Date fecha;
+		try {
+			SimpleDateFormat form = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
+			fecha = form.parse(date);
+		}catch (ParseException e){
+			//error en el parseo de la fecha por no tener esa estructura
+			//se pasa una fecha por defecto, que va a ser la actual
+			fecha = new Date();
+		}
+		return fecha;
+	}
+
+}	
+
+
 
 }	
 
